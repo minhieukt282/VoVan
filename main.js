9317ddf35363d62f3979a26dc8ab78aa8c66d23d
minhieukt <minhieukt@gmail.com>
2022-10-11 14:10:22 +0700

    sua ket qua

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ function main() {
     let inputFirst = document.getElementById("input").value
     let inputSecond = document.getElementById("needed").value
     let inputVolume = document.getElementById("volume").value
-    let firstVolume = (inputSecond / inputFirst) * inputVolume
-    let waterVolume = inputVolume - firstVolume
-    let mass = 0.826 * firstVolume
-    let waterMass = waterVolume
+    let firstVolume = Math.round((inputSecond / inputFirst) * inputVolume * 100) / 100
+    let waterVolume = Math.round((inputVolume - firstVolume) * 100) / 100
+    let mass = Math.round(0.826 * firstVolume * 100) / 100
+    let waterMass = Math.round(waterVolume * 100) / 100
     screen = `Thể tích nồng độ ban đầu: ${firstVolume} <br>
-    Thể tích nước: ${waterVolume}<br>
+    Thể tích nước: ${waterVolume}<br><br><br>
     Khối lượng nồng độ cồn ban đầu: ${mass}<br>
     Khối lượng nước: ${waterMass}`
     document.getElementById("result").innerHTML = screen
