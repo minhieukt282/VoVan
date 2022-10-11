d3c0a17dbcf39352e60563be51dec799178c5c0d
minhieukt <minhieukt@gmail.com>
2022-10-11 14:15:29 +0700

    sua ket qua

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ function main() {
     let waterVolume = Math.round((inputVolume - firstVolume) * 100) / 100
     let mass = Math.round(0.826 * firstVolume * 100) / 100
     let waterMass = Math.round(waterVolume * 100) / 100
-    screen = `Thể tích nồng độ ban đầu: ${firstVolume} <br>
-    Thể tích nước: ${waterVolume}<br><br><br>
-    Khối lượng nồng độ cồn ban đầu: ${mass}<br>
+    screen = `-----Tính theo thể tích----- <br>
+    Thể tích nồng độ ban đầu: ${firstVolume} <br>
+    Thể tích nước: ${waterVolume}<br><br>
+    -----Tính theo khối lượng----- <br>
+    Khối lượng nồng độ cồn (25^C): ${mass}<br>
     Khối lượng nước: ${waterMass}`
     document.getElementById("result").innerHTML = screen
 }
